85c0238ae08ae195ae8046e11c906be63b9b530b
Paul-Int <pawel.wilusz1334@gmail.com>
2025-11-29 08:31:25 +0100

    Delete 7.4.py

--- 7.4.py
+++
@@ -1,7 +0,0 @@
-##
-# program thath tells if a tree can be cut down
-#
-
-height = input('Enter tree circumference in cm: ')
-can_be_cut = int(height) >= 50  
-print(f'Tree can be cut down: {can_be_cut}')
