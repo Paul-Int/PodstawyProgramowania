5b91134169c63d4f669b0d5fe7eb411e92f0024b
Paul-Int <pawel.wilusz1334@gmail.com>
2025-11-29 08:29:05 +0100

    Delete 5.5.py

--- 5.5.py
+++
@@ -1,8 +0,0 @@
-##
-# calculating price with discount and the difference between prices
-price = float(input('Enter price:  '))
-discount = float(input('Enter discount %:  '))
-discountedPrice = price - (price * discount / 100)
-difference = price - discountedPrice
-print(f'Price with discount: {discountedPrice:.2f}')
-print(f'Reduction: {difference:.2f}')
