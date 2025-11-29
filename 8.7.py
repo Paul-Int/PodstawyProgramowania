49ccd3ce99b7b3952261b8fc2d2feb31b9bc1c9b
Paul-Int <pawel.wilusz1334@gmail.com>
2025-11-29 08:33:00 +0100

    Delete 8.7.py

--- 8.7.py
+++
@@ -1,8 +0,0 @@
-###
-# a program that converts decimal number to binary and hexadecimal
-#
-decimal = int(input('Enter a decimal number: '))
-binary = bin(int(decimal))
-hexadecimal = hex(int(decimal))
-print(f'Binary number: {binary} ')
-print(f'Hexadecimal number: {hexadecimal} ')
